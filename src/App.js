b4d1c7ea61051fb8c19643de7f0fe9a731b68810
Jim Wheaton <jim@getstat.com>
2017-09-26 21:06:51 -0700

    Basic blackbox barchart

--- src/App.js
+++ src/App.js
@@ -6,9 +6,41 @@ import d3logo from './d3logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    data: {
+      current: []
+    }
+  }
+  componentDidMount() {
+    fetch('data.json')
+    .then(response => response.json())
+    .then(data => this.setState(this.processData(data)))
+  }
+  processData(data) {
+    const [ min, max ] = [10, 10000]
+    const current = Object.keys(data.current).map(feature => {
+      let total = data.current[feature].total_traffic
+      total = isNaN(total) || total < 50 
+        ? Math.random() * (max - min) + min
+        : total
+
+      return {
+        feature,
+        total
+      }
+    })
+
+    return {
+      data: {
+        current
+      }
+    }
+  }
   render() {
+    const data = this.state.data
+
     return (
-      <div className="App">
+      <div className="App container">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <img src={d3logo} className="App-logo" alt="logo" />
@@ -21,7 +53,9 @@ class App extends Component {
             </Col>
           </Row>
           <Row>
-            <Col xs={9} md={6}><BlackboxBarChart /></Col>
+            <Col xs={9} md={6}><BlackboxBarChart data={data.current}
+                                                 width={500}
+                                                 height={500} /></Col>
             <Col xs={9} md={6}>Second column</Col>
           </Row>
         </Grid>
--- src/blackbox/BarChart.js
+++ src/blackbox/BarChart.js
@@ -1,9 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { scaleLinear } from 'd3-scale'
+import { max } from 'd3-array'
+import { select } from 'd3-selection'
 
 class BlackboxBarChart extends Component {
+    constructor(props) {
+        super(props)
+        this.createBarChart = this.createBarChart.bind(this)
+    }
+
+    componentDidMount() {
+        this.createBarChart()
+    }
+
+    componentDidUpdate() {
+        this.createBarChart()
+    }
+
+    createBarChart() {
+        const padding = 20;
+        const node = this.node
+        const { data, height } = this.props
+        const dataMax = max(data, d => d.total)
+        const yScale = scaleLinear()
+            .domain([0, dataMax])
+            .range([0, height - padding])
+
+        select(node)
+            .selectAll("rect")
+            .data(data)
+            .enter()
+            .append("rect")
+
+        select(node)
+            .selectAll("rect")
+            .data(data)
+            .exit()
+            .remove()
+
+        select(node)
+            .selectAll("rect")
+            .data(data)
+            .attr("x", (d, i) => i * 25)
+            .attr("y", d => height - yScale(d.total))
+            .attr("height", d => yScale(d.total))
+            .attr("width", 25)
+    }
     render() {
+        const { width, height } = this.props
         return (
-            <div>Blackbox bar chart</div>
+            <svg ref={node => this.node = node}
+                width={width}
+                height={height}>
+            </svg>
         )
     }
 }
